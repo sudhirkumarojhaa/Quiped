4b8f72a799874c4c0a9038a408b80f565bf80752
James Sudhir <jamessudhir@Sudhir-MacBook-Air.local>
2020-03-26 18:26:58 +0530

    Updates

---
+++ precache-manifest.89a0ab05bd6e117b78ec252495d2defd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f27ec8a00c8de53a19f7a7124d61ee",
+    "url": "/sudhirkumarojhaa/Quiped/index.html"
+  },
+  {
+    "revision": "f3afd6ff1667b6c6f418",
+    "url": "/sudhirkumarojhaa/Quiped/static/css/main.6d481f32.chunk.css"
+  },
+  {
+    "revision": "bbc45cc6ca7feb41064b",
+    "url": "/sudhirkumarojhaa/Quiped/static/js/2.a2a4507e.chunk.js"
+  },
+  {
+    "revision": "f3afd6ff1667b6c6f418",
+    "url": "/sudhirkumarojhaa/Quiped/static/js/main.6487ed54.chunk.js"
+  },
+  {
+    "revision": "25dd0a5554eb0142d8d2",
+    "url": "/sudhirkumarojhaa/Quiped/static/js/runtime~main.cff4c110.js"
+  },
+  {
+    "revision": "584b607f5c2ff075429dc0e7b8d142ef",
+    "url": "/sudhirkumarojhaa/Quiped/static/media/loader.584b607f.gif"
+  }
+]);
